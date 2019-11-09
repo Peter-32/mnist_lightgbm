bf0dc5b490ed35acf0ec6ce528d5bf4a302f16a3
Peter-32 <peter@impactradius.com>
2019-11-09 14:10:31 -0800

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Based on ideas from http://data-in-model-out.com/xgboost-feature-engineering-using-neural-networks/',
+    author='Peter J Myers',
+    license='MIT',
+)
